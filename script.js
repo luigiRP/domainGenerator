f8b93d05cedf5b93d749b6a37e896a182d49d6b9
luigiRP <luigirosales84@gmail.com>
2020-07-23 15:22:16 +0000

    second

--- script.js
+++ script.js
@@ -1,20 +1,48 @@
+window.onload = () => {
+
+
 let pronoun=['the','our','her','his'];
-let end=['bird','penguin','tiger','lion','parrot'];
 let mid=['great','big','small','medium'];
-let extension=['.com','.net','.org','.co'];
-
-
+let end=['bird','penguin','tiger','lion','parrot','sonicorg'];
+let extension=['com','net','org','co'];
+let varr=[];
+var list = document.getElementsByTagName("UL");
 
 for (i=0;i<pronoun.length;i++) {
     for (j=0;j<mid.length;j++) {
         for (k=0;k<end.length;k++) {
             for (l=0;l<extension.length;l++) {
-                console.log(pronoun[i]+mid[j]+end[k]+extension[l]);
+               if (end[k].endsWith(extension[l])) {
+                   let finalEnding=[];
+                   let nending=end[k];
+                   let nextension=extension[l];
+                   
+                   for (let index=0;index<nending.length-nextension.length;index++) {
+                        finalEnding.push(nending[index])
+                   }
+                   finalEnding=finalEnding.toString()
+                   finalEnding=finalEnding.replace(/,/g, "");
+                   varr.push(pronoun[i].concat(mid[j],finalEnding,'.',extension[l]))
+               }
+               else {
+                    varr.push(pronoun[i].concat(mid[j],end[k],'.',extension[l]));
+               }
+                
             }
         }
     }
     
 }
+
+for (let i =0; i <= varr.length-1; i++) {
+  let divElem = document.getElementById("list");
+let myNewHOne = document.createElement("li");
+let t = document.createTextNode(varr[i]);
+myNewHOne.appendChild(t);  
+divElem.appendChild(myNewHOne);
+}
+}
+
 
 
 
